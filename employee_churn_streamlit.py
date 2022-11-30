77de49cee4e8a8bf9c1d9cbaaa98c043e54f0f7b
francocibils <108623588+francocibils@users.noreply.github.com>
2022-11-30 10:36:26 -0300

    Update employee_churn_streamlit.py

--- employee_churn_streamlit.py
+++ employee_churn_streamlit.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 # Load model
-rf_clf = pickle.load(open('employee-churn/rf_clf.pkl', 'rb'))
+rf_clf = pickle.load(open('rf_clf.pkl', 'rb'))
 
 # Naming
 areas = ['Product management', 'Technical', 'Sales', 'Management', 'Accounting', 'R+D', 'Marketing', 'HHRR', 'Support', 'IT']
